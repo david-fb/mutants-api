97145aae96de1f87f04e89b16753924297fb758e
David Basto <davidbasto01@gmail.com>
2022-10-13 19:48:20 -0500

    update schema

--- schemas/mutant.schema.js
+++ schemas/mutant.schema.js
@@ -3,11 +3,12 @@ const Joi = require("joi");
 const id = Joi.number().integer();
 const name = Joi.string().min(1);
 const alias = Joi.string().min(1);
-const type = Joi.string().valid("Heroe", "Villano");
-const condition = Joi.string().valid("Libertad", "Detenido", "Desconocido");
+const type = Joi.string().valid("hero", "villain");
+const condition = Joi.string().valid("freedom", "arrested", "unknown");
 const image = Joi.string().uri();
 const vehicle_id = Joi.number().integer();
-const place_id = Joi.number().integer();
+const placeId = Joi.number().integer();
+const powersId = Joi.array().items(Joi.number().integer()).unique();
 
 const createMutantSchema = Joi.object({
   name: name.required(),
@@ -16,7 +17,8 @@ const createMutantSchema = Joi.object({
   condition: condition.required(),
   image: image.required(),
   vehicle_id,
-  place_id: place_id.required(),
+  powersId,
+  placeId: placeId.required(),
 });
 
 const updateMutantSchema = Joi.object({
@@ -26,13 +28,20 @@ const updateMutantSchema = Joi.object({
   condition,
   image,
   vehicle_id,
-  place_id,
+  placeId,
+  powersId,
+});
+
+const getMutantByIdSchema = Joi.object({
+  id: id.required(),
 });
 
 const getMutantSchema = Joi.object({
-  id,
-  name,
-  place: Joi.string().min(1),
-}).xor("id", "name", "place");
-
-module.exports = { createMutantSchema, updateMutantSchema, getMutantSchema };
+  query: Joi.string().min(1).required(),
+});
+module.exports = {
+  createMutantSchema,
+  updateMutantSchema,
+  getMutantSchema,
+  getMutantByIdSchema,
+};
