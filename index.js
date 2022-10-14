95080b8de2b8280b080188b08efe35c7f6814afd
David Basto <davidbasto01@gmail.com>
2022-10-13 19:46:56 -0500

    add error middlewares

--- index.js
+++ index.js
@@ -3,21 +3,26 @@ const express = require("express");
 const app = express();
 const PORT = config.port;
 const routerApi = require("./routes");
-const sequelize = require("./libs/sequelize");
+
+const {
+  logErrors,
+  errorHandler,
+  boomErrorHandler,
+  ormErrorHandler,
+} = require("./middlewares/errorHandler");
 
 app.use(express.json());
 
 app.get("/", async (req, res) => {
-  try {
-    await sequelize.authenticate();
-    console.log("Connection has been established successfully.");
-  } catch (error) {
-    console.error("Unable to connect to the database: ", error);
-  }
-  res.send("Hello world");
+  res.send("Mutants Api");
 });
 
 routerApi(app);
+
+app.use(logErrors);
+app.use(boomErrorHandler);
+app.use(ormErrorHandler);
+app.use(errorHandler);
 
 app.listen(PORT, () => {
   console.log(`Running on http://localhost:${PORT}`);
